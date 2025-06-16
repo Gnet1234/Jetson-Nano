c0e9068af790a75b257f9a43296039490a3add12
Gnet1234 <garnetmax02@gmail.com>
2025-06-16 15:39:37 -0400

    Add files via upload

---
+++ yolov11_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+xhost +
+
+docker run -it \
+--gpus all \
+--net=host \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--env="NVIDIA_VISIBLE_DEVICES=all" \
+--env="NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics,video" \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /home/jetson/temp:/ultralytics/ultralytics/temp \
+--device=/dev/video0 \
+-p 9090:9090 \
+-p 8888:8888 \
+yahboomtechnology/ultralytics:1.0.3 /bin/bash
